50c33eb8118a4d85cfd5465229a3391e6106a5cf
brontasaurus <bronte.kunkler@gmail.com>
2020-08-19 20:00:22 -0700

    middle of fortune kook
    oolg

---
+++ src/_07_fortune_cookie/koook.java
@@ -0,0 +1,33 @@
+package _07_fortune_cookie;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class koook {
+        public void showButton() {
+            JFrame frame = new JFrame();
+            frame.setVisible(true);
+            JButton button = new JButton();
+            frame.add(button);
+            frame.pack();
+            button.addActionListener(this);
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+       }
+    }
+	
+
+
